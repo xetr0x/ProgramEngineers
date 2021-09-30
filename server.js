5d1002c6d0009cfaca627404106e033408feb46f
kevin wilusz <kwilusz10@gmail.com>
2021-09-30 12:13:46 +0200

    trying to make the website

---
+++ server.js
@@ -0,0 +1,23 @@
+'using strict'
+
+const express = require('express' );
+const app = express();
+const fs = require('fs');
+const PORT = process.env.PORT|| 8080;
+
+
+
+
+app.get('/', (req,res) => {
+    fs.readFile('index.html', (err, data) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', data.length);
+    res.send(data);
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
