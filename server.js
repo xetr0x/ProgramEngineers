8f295e2756a18747054e0b36ff86a892f637e1c8
kevin wilusz <kwilusz10@gmail.com>
2021-10-04 11:34:31 +0200

    Bigger changed to the file hierarchy

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'using strict'
+'use strict';
 
 const express = require('express' );
 
@@ -6,9 +6,9 @@ const app = express();
 
 const fs = require('fs');
 
+app.use(express.static('public'));
 
-
-
+/*
 app.get('/stylesheet.css', (req,res) => {
     fs.readFile('stylesheet.css', (err, data) => {
         res.statusCode = 200;
@@ -18,14 +18,7 @@ app.get('/stylesheet.css', (req,res) => {
     })
 })
 
-app.get('/kevins_site/kevincss.css', (req,res) => {
-    fs.readFile('kevins_site/kevincss.css', (err, data) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/css');
-        //res.setHeader('Content-Length', data.length);
-        res.send(data);
-    })
-})
+
 
 app.get('/img/exempelbild.jpg', (req,res) => {
     fs.readFile('img/exempelbild.jpg', (err, data) => {
@@ -44,24 +37,7 @@ app.get('/img/logo.png', (req,res) => {
         res.send(data);
     })
 })
-app.get('/kevins_site/portrait.png', (req,res) => {
-    fs.readFile('img/portrait3.png', (err, data) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'image/png');
-        //res.setHeader('Content-Length', data.length);
-        res.send(data);
-    })
-})
-
-
-app.get('/kevins_site/kevin.html', (req,res) => {
-    fs.readFile('kevins_site/kevin.html', (err, data) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        //res.setHeader('Content-Length', data.length);
-        res.send(data);
-    })
-})
+*/
 
 app.get('/', (req,res) => {
     fs.readFile('index.html', (err, data) => {
@@ -73,8 +49,8 @@ app.get('/', (req,res) => {
 
 
 
-const PORT = process.env.PORT|| 8080;
+const port = process.env.PORT|| 8080;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
 })
